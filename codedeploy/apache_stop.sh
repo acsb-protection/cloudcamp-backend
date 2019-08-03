587bf4b8e6b92fd2e128d44a82097ecafc464087
mnetobr <53312970+mnetobr@users.noreply.github.com>
2019-08-03 10:06:39 -0300

    Update apache_stop.sh

--- codedeploy/apache_stop.sh
+++ codedeploy/apache_stop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
  
 #Stop no servidor web apache2
+a2enmod headers
 service apache2 stop
-if [[ -f /var/www/cloudcamp-backend ]]; then
-    echo "Removendo DIR"
-    rm -rf /var/www/cloudcamp-backend
+if [ ! -d '/var/www/cloudcamp-backend' ]
+then
+  echo "Caminho nao existe..."
+else
+  rm -rf '/var/www/cloudcamp-backend'
 fi
